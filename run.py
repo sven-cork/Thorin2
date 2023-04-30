77ad5a4a68c37c0a8e95188623ee89b01c0e8c67
sven-cork <>
2023-04-30 19:21:02 +0000

    Deploy to Heroku via CLI

--- run.py
+++ run.py
@@ -1,9 +1,12 @@
 import os
 import json
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
+if os.path.exists("env.py"):
+    import env
 
 
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
 
 
 @app.route("/")
@@ -19,12 +22,15 @@ def about():
     return render_template("about.html", page_title="About", company=data)
 
 
-@app.route("/contact")
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
+    if request.method == "POST":
+        flash("Thanks {}, we have received your message!".format(
+            request.form.get("name")))
     return render_template("contact.html", page_title="Contact")
 
 
-@app.route("/careers")
+@ app.route("/careers")
 def careers():
     return render_template("careers.html", page_title="Careers")
 
